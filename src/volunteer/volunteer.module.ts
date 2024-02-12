53894e30228fb08b63b3890beee456408c727727
safimiazi <mohibullamiazi@gmail.com>
2024-02-12 19:46:35 +0600

    pretear added

--- src/volunteer/volunteer.module.ts
+++ src/volunteer/volunteer.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { VolunteerController } from './volunteer.controller';
 import { VolunteerService } from './volunteer.service';
