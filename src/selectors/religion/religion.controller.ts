fb2d56ef76921f628abe13d3948212f2faba0769
safimiazi <mohibullamiazi@gmail.com>
2024-02-15 11:45:24 +0600

    religion type get function addded

--- src/selectors/religion/religion.controller.ts
+++ src/selectors/religion/religion.controller.ts
@@ -1,6 +1,7 @@
+/* eslint-disable prettier/prettier */
 import { Body, Controller, Get, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { ReligionService } from './religion.service';
-import { createReligionDto } from './create-religion.dto';
+import { CreateReligionDto } from './create-religion.dto';
 import { Religion } from './religion.entity';
 
 @Controller('religion')
@@ -9,7 +10,7 @@ export class ReligionController {
     constructor(private readonly religionService: ReligionService){}
 
     @Post()
-    async createReligion(@Body() createReligionDto: createReligionDto): Promise<{ message: string, religion: Religion }> {
+    async createReligion(@Body() createReligionDto: CreateReligionDto): Promise<{ message: string, religion: Religion }> {
         try {
             const religion = await this.religionService.createReligion(createReligionDto);
             return { message: 'User created successfully', religion };
@@ -17,5 +18,15 @@ export class ReligionController {
             throw new HttpException({ message: 'An unexpected error occurred.' }, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+@Get()
+async getAllReligion():Promise<{message: string; religionType: Religion[] }> {
+    try {
+        const allReligion = await this.religionService.getAllReligion();
+        return {message: "Successfully get all Religion types", religionType: allReligion}
+    } catch (error) {
+        throw new HttpException({message: "Un expected error occurred"}, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+}
 
 }
