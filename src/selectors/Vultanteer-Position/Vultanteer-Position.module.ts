506ee7e9d306b2e64511fadf200e2843706cb3b9
ashikurbd71 <ashikurovi2003@gmail.com>
2024-02-18 17:22:43 +0600

    add vulanteer module

--- src/selectors/Vultanteer-Position/Vultanteer-Position.module.ts
+++ src/selectors/Vultanteer-Position/Vultanteer-Position.module.ts
@@ -1,9 +1,12 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VulanteerPositionController } from './Vulanteer-Position.controller';
+import { VulanteerPosition } from './Vultanteer-Position.entity';
+import { VulanteerPositionService } from './Vultanteer-Position.service';
 
 @Module({
-
-
-
+  imports: [TypeOrmModule.forFeature([VulanteerPosition])],
+  controllers: [VulanteerPositionController],
+  providers: [VulanteerPositionService]
 })
-
-export class Vulanteer-Position {}
+export class VulanteerPositionModule {}
